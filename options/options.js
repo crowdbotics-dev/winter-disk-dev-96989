568906508d44cb4760310c5e780214450ca17cee
Crowdbotics <engineering@crowdbotics.com>
2023-07-03 23:17:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-disk-dev-96989",
+  url: "https://winter-disk-dev-96989.botics.co",
+  api: "https://winter-disk-dev-96989.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
